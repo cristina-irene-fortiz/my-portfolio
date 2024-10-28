5ace6c347b38f67484d09c0f39c73bf21f94035e
Cristina Irene Fortiz <cfortiz1@jhu.edu>
2024-10-28 16:55:28 +0100

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio!</h1>
+      <p>Hi, I'm Cristina, a passionate problem solver with a background in Cybersecurity and Data Science.</p>
+      <p>Explore my projects and learn more about me!</p>
+    </div>
+  );
+};
+
+export default Home;
