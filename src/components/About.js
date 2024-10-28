974477810bfb6bde4a610df0227369bdfc096d7d
Cristina Irene Fortiz <cfortiz1@jhu.edu>
2024-10-28 16:55:49 +0100

    Create About.js

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>I am a JHU Graduate Student pursuing an MS in Cybersecurity (2021) and an MS in Computer Science (~2025).</p>
+      <p>With 8+ years of experience as a Cyber Analyst, I am also a Data Science, AI, and ML Enthusiast.</p>
+    </div>
+  );
+};
+
+export default About;
