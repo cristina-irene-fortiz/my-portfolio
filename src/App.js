dac43aaf3d34f3aa81d03cfd4fcad6babe928bf4
Cristina Irene Fortiz <cfortiz1@jhu.edu>
2025-01-12 12:38:09 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Projects from './components/Projects';
+import { AppBar, Toolbar, Typography, Button, Container, Grid, Card, CardContent } from '@mui/material';
 
 function App() {
   return (
     <Router basename="/my-portfolio">
       <div>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/projects">Projects</Link></li>
-          </ul>
-        </nav>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Cristina's Portfolio
+            </Typography>
+            <Button color="inherit" component={Link} to="/">Home</Button>
+            <Button color="inherit" component={Link} to="/about">About</Button>
+            <Button color="inherit" component={Link} to="/projects">Projects</Button>
+          </Toolbar>
+        </AppBar>
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
+        <Container style={{ marginTop: '20px' }}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+          </Routes>
+        </Container>
       </div>
     </Router>
   );
 }
+
+const HomePage = () => (
+  <Typography variant="h4" gutterBottom>
+    Welcome to My Portfolio!
+    <Typography variant="body1">
+      Hi, I'm Cristina, a data scientist and cybersecurity expert. Explore my work below!
+    </Typography>
+  </Typography>
+);
+
+const AboutPage = () => (
+  <Typography variant="h4" gutterBottom>About Me</Typography>
+);
+
+const ProjectsPage = () => (
+  <Grid container spacing={3}>
+    <Grid item xs={12} sm={6} md={4}>
+      <Card>
+        <CardContent>
+          <Typography variant="h5">Project 1</Typography>
+          <Typography>Details about your project.</Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  </Grid>
+);
 
 export default App;
