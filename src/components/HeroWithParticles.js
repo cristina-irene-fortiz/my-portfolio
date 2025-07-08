c31a33aa93253f9086bf201083a8f013c63ef680
Cristina Irene Fortiz <cfortiz1@jhu.edu>
2025-07-08 19:24:27 +0000

    testtest

--- src/components/HeroWithParticles.js
+++ src/components/HeroWithParticles.js
@@ -108,7 +108,7 @@ export default function HeroWithParticles() {
           </Grid>
 
           {/* Larger animated portrait */}
-          <Grid item xs={12} md={7}>
+          <Grid item xs={12} md={9}>
             <Box
               component={motion.img}
               src="./profile.jpg"
